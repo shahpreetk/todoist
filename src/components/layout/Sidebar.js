4729551e5ec3cf7c012ffdd21fc613f704868843
Preet Shah <shahpreetk@gmail.com>
2020-08-30 23:28:20 +0530

    firebase.js and sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import {FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar} from 'react-icons/fa'
 
 const Sidebar = () => {
     return(
-        <p>i am a sidebar</p>
+        <>
+            <div className="sidebar" data-testid='sidebar'>
+                <ul className="sidebar__generic">
+                    <li><span><FaInbox /></span><span>Inbox</span></li>
+                    <li><span><FaRegCalendar /></span><span>Today</span></li>
+                    <li><span><FaRegCalendarAlt /></span><span>Next 7 Days</span></li>
+                </ul>
+
+                <div className="sidebar__middle">
+                    <span><FaChevronDown /></span>
+                    <h2>Projects</h2>
+                </div>
+
+                <ul className="sidebar__projects">
+                    Projects will be here!
+                </ul>
+                Add project component here!
+            </div>
+        </>
     )
 }
 
