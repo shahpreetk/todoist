34b60bed16cc6e50f7985af834d46bffc80f37da
Preet Shah <shahpreetk@gmail.com>
2020-08-31 11:47:57 +0530

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = firebase.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messageSenderId: '',
-    apiId: ''
+    apiKey: "AIzaSyAXF3E7hzdxDTWVlESEt6IzTjAAJmrB8SE",
+    authDomain: "todoist-f1762.firebaseapp.com",
+    databaseURL: "https://todoist-f1762.firebaseio.com",
+    projectId: "todoist-f1762",
+    storageBucket: "todoist-f1762.appspot.com",
+    messagingSenderId: "229473655997",
+    appId: "1:229473655997:web:09ed3b2ce2e1f65797abd4"
 });
 
 export {firebaseConfig as firebase};
