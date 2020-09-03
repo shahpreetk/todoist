943fdaa684f3efc665ac1ca47ffa4be6ebac87e2
Preet Shah <shahpreetk@gmail.com>
2020-09-03 23:17:04 +0530

    AddProject component

---
+++ src/components/AddProjects.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FaDivide } from 'react-icons/fa'
+
+const AddProject = () => {
+    return (
+        <div>I am add project</div>
+    )
+}
+
+export default AddProject
