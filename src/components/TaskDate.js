3b91b40ddbac368ab7928f517b82b5c1056cfb4f
Preet Shah <shahpreetk@gmail.com>
2020-09-09 19:27:21 +0530

    fixed spacing issue

--- src/components/TaskDate.js
+++ src/components/TaskDate.js
@@ -10,21 +10,21 @@ const TaskDate = ({setTaskDate, showTaskDate, setShowTaskDate}) => showTaskDate 
                 setTaskDate(moment().format('DD/MM/YYYY'))
             }} data-testid='task-date-overlay'>
                 <span><FaSpaceShuttle /></span>
-                <span>Today</span>
+                <span>&nbsp;&nbsp;Today</span>
             </li>
             <li onClick={()=>{
                 setShowTaskDate(false)
                 setTaskDate(moment().add(1,'day').format('DD/MM/YYYY'))
             }} data-testid='task-date-tomorrow'>
                 <span><FaSun /></span>
-                <span>Tomorrow</span>
+                <span>&nbsp;&nbsp;Tomorrow</span>
             </li>
             <li onClick={()=>{
                 setShowTaskDate(false)
                 setTaskDate(moment().add(7,'days').format('DD/MM/YYYY'))
             }} data-testid='task-date-next-week'>
                 <span><FaRegPaperPlane /></span>
-                <span>Next Week</span>
+                <span>&nbsp;&nbsp;Next Week</span>
             </li>
         </ul>
     </div>
