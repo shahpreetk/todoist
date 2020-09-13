a0d8b36e370b2381e20cfe29a8ada002beda3cf7
Preet Shah <shahpreetk@gmail.com>
2020-09-14 00:37:02 +0530

    add projects test

--- src/__tests__/Tasks.spec.js
+++ src/__tests__/Tasks.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import { Tasks } from '../components/Tasks';
+import Tasks from '../components/Tasks';
 import { useSelectedProjectValue } from '../context';
 
 jest.mock('../context', () => ({
